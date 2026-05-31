4a8c7cb75d849e82b7acc96d575e7a79bea93d0c
DEVELOPERBibek10 <bibekpyakurel7@gmai.com>
2026-05-31 11:03:08 +0545

    refactor: update like schema to use likableId and likableType; remove video and comment fields

--- src/models/like.model.ts
+++ src/models/like.model.ts
@@ -1,41 +1,33 @@
 import mongoose, { Schema } from "mongoose";
+import { ApiError } from "../utils/ApiError.js";
+import type { NextFunction } from "express";
 
 const likeSchema = new Schema(
   {
-    video: {
-      type: Schema.Types.ObjectId,
-      ref: "Video",
-    },
-    comment: {
-      type: Schema.Types.ObjectId,
-      ref: "Comment",
-    },
     likedBy: {
       type: Schema.Types.ObjectId,
       ref: "User",
+      required: true,
+    },
+    likableId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    likableType: {
+      type: String,
+      enum: ["Video", "Comment"],
+      required: true,
     },
   },
   { timestamps: true }
 );
 
 likeSchema.index(
-  { likedBy: 1, video: 1 },
+  { likedBy: 1, likableId: 1 },
   {
     unique: true,
-    partialFilterExpression: { video: { $exists: true } },
   }
 );
 
-likeSchema.index(
-  { likedBy: 1, comment: 1 },
-  {
-    unique: true,
-    partialFilterExpression: { comment: { $exists: true } },
-  }
-);
-
-likeSchema.index({ video: 1 });
-likeSchema.index({ comment: 1 });
-
-
+likeSchema.index({ likableId: 1 });
 export const Like = mongoose.model("Like", likeSchema);
