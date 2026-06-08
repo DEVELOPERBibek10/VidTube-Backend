9c903046b7ffe313077ae563827af0a02bda5e72
DEVELOPERBibek10 <bibekpyakurel7@gmai.com>
2026-06-08 08:59:04 +0545

    fix: streamline GlobalError type by removing unused error types

--- src/types/Error/GobalError.ts
+++ src/types/Error/GobalError.ts
@@ -1,14 +1,12 @@
 import type { Error as MongooseError } from "mongoose";
-import type { MongoDuplicateKeyError } from "./mongooseError.js";
-import type { JsonParseError } from "./jsonParserError.js";
 import type { MulterError } from "multer";
 import type { ApiError } from "../../utils/ApiError.js";
 
 export type GlobalError =
   | ApiError
   | MongooseError.ValidationError
-  | MongoDuplicateKeyError
   | MongooseError.CastError
   | MulterError
-  | JsonParseError
-  | Error;
+  | SyntaxError
+  | Error
+  | any;
