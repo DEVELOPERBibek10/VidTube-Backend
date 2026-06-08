474af342c9ea597ffa4e5e7cad19e2e43bb6d79c
DEVELOPERBibek10 <bibekpyakurel7@gmai.com>
2026-06-08 12:39:03 +0545

    fix: ensure sameSite attribute is consistently set for cookie options

--- src/constants/cookieOption.ts
+++ src/constants/cookieOption.ts
@@ -4,10 +4,12 @@ export const accessTokenOptions: CookieOptions = {
   httpOnly: true,
   secure: true,
   maxAge: 24 * 60 * 60 * 1000,
+  sameSite: "none",
 };
 
 export const refreshTokenOptions: CookieOptions = {
   httpOnly: true,
   secure: true,
   maxAge: 8 * 24 * 60 * 60 * 1000,
+  sameSite: "none",
 };
