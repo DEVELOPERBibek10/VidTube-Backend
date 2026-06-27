eb548105fb6c1dab205918eccdd92865dfe69f68
DEVELOPERBibek10 <bibekpyakurel7@gmai.com>
2026-06-27 15:09:56 +0545

    fix: use a consistent port variable for server listening

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import "dotenv/config";
 import { connectRedis } from "./db/redis.js";
 import app from "./app.js";
 import connectDB from "./db/index.js";
+const port = process.env.PORT || 8000;
 
 async function server() {
   try {
     const database = await Promise.all([connectDB(), connectRedis()]);
     if (database.every((db) => db)) {
-      const server = app.listen(process.env.PORT, () => {
-        console.log(`Server running on http://localhost:${process.env.PORT}`);
+      const server = app.listen(port, () => {
+        console.log(`Server running on http://localhost:${port}`);
       });
       server.on("error", (err) => {
         console.error("EXPRESS SERVER FAILURE: Could not bind to port :", err);
