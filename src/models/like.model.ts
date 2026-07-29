13d5fd1f21d211bad952775862c9d90f840db108
DEVELOPERBibek10 <bibekpyakurel7@gmai.com>
2026-07-29 22:40:18 +0545

    feat: enhance Like model schema with type safety and improved enum usage

--- src/models/like.model.ts
+++ src/models/like.model.ts
@@ -1,6 +1,8 @@
 import mongoose, { Schema } from "mongoose";
+import type { LikeSchema } from "../types/Model/Like.js";
+import { likeableType as LikeableType } from "../types/Model/Like.js";
 
-const likeSchema = new Schema(
+const likeSchema = new Schema<LikeSchema>(
   {
     likedBy: {
       type: Schema.Types.ObjectId,
@@ -11,9 +13,9 @@ const likeSchema = new Schema(
       type: Schema.Types.ObjectId,
       required: true,
     },
-    likableType: {
+    likeableType: {
       type: String,
-      enum: ["Video", "Comment"],
+      enum: Object.values(LikeableType),
       required: true,
     },
   },
@@ -21,11 +23,11 @@ const likeSchema = new Schema(
 );
 
 likeSchema.index(
-  { likedBy: 1, likable: 1 },
+  { likedBy: 1, likable: 1, likeableType: 1 },
   {
     unique: true,
   }
 );
 
-likeSchema.index({ likable: 1 });
+likeSchema.index({ likable: 1, likeType: 1 });
 export const Like = mongoose.model("Like", likeSchema);
