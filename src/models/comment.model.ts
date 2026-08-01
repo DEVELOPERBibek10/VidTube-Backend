6be4523a2b3cfaa66d9d27a9b6c7bd427f2395c5
DEVELOPERBibek10 <bibekpyakurel7@gmai.com>
2026-08-01 23:02:57 +0545

    add: parentComment field for reply comment feature.
    fix: add type defination of the schema.

--- src/models/comment.model.ts
+++ src/models/comment.model.ts
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import type { IComment } from "../types/Model/Comment.js";
 
-const commentSchema = new Schema(
+const commentSchema = new Schema<IComment>(
   {
     content: {
       type: String,
@@ -10,6 +11,11 @@ const commentSchema = new Schema(
       type: Schema.Types.ObjectId,
       ref: "Video",
     },
+    parentComment: {
+      type: Schema.Types.ObjectId,
+      default: null,
+      ref: "Comment",
+    },
     owner: {
       type: Schema.Types.ObjectId,
       ref: "User",
@@ -18,5 +24,4 @@ const commentSchema = new Schema(
   { timestamps: true }
 );
 
-
-export const Comment = mongoose.model("Comment", commentSchema);
+export const Comment = mongoose.model<IComment>("Comment", commentSchema);
