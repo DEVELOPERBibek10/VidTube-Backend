d523de49a2a16255a5c36ad30468531eb12c5d5b
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2026-09-03 09:25:08 +0545

    fix: update 'TypedReques't by more strict type checking and remove type checking in file upload for un-auth routes, use MongoId in the 'UserRequest' type and more strict type checking in the 'AuthTypedRequest' type.

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,20 +1,18 @@
 import type { ParamsDictionary } from "express-serve-static-core";
 import type { Request } from "express";
 import type { ParsedQs } from "qs";
-import type { Types } from "mongoose";
+import type { MongoId } from "./id.js";
 
 export interface TypedRequest<
-  TBody = any,
-  TFiles = Express.Multer.File | Express.Multer.File[],
-  TParams = ParamsDictionary,
-> extends Omit<Request, "body" | "files" | "params"> {
+  TBody = Record<string, any>,
+  TParams extends ParamsDictionary = ParamsDictionary,
+> extends Omit<Request, "body" | "params"> {
   body: TBody;
   params: TParams;
-  files?: TFiles;
 }
 
 interface UserRequest {
-  _id: string | Types.ObjectId;
+  _id: MongoId;
   username: string;
   email: string;
   fullName: string;
@@ -31,14 +29,15 @@ interface UserRequest {
 }
 
 export interface AuthTypedRequest<
-  TBody = any,
-  TFile = Express.Multer.File | Express.Multer.File[],
-  TParams = ParamsDictionary,
-  TQuery = ParsedQs,
+  TBody = Record<string, any>,
+  TFile extends Express.Multer.File | Express.Multer.File[] | null =
+    Express.Multer.File | Express.Multer.File[],
+  TParams extends ParamsDictionary | null = ParamsDictionary | null,
+  TQuery extends ParsedQs = ParsedQs,
 > extends Omit<Request, "body" | "files" | "params" | "query"> {
   body: TBody;
   user: UserRequest;
   params: TParams;
   query: TQuery;
-  files?: TFile;
+  files: TFile;
 }
