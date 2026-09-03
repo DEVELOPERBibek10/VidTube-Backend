b046010c49024a6c3eecb7fc383ecba523a43dc5
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2026-09-03 09:25:46 +0545

    fix: rename 'parentComment' to 'parentId' in IComment interface for consistency and add likes type

--- src/types/Model/Comment.ts
+++ src/types/Model/Comment.ts
@@ -3,8 +3,9 @@ import { type Types } from "mongoose";
 export interface IComment {
   content: string;
   video: Types.ObjectId;
-  parentComment: Types.ObjectId | null;
+  parentId: Types.ObjectId | null;
   owner: Types.ObjectId;
+  likes: number;
   createdAt: Date;
   updatedAt: Date;
 }
