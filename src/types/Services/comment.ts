e80f822a3f57dd294f9affb80a716da3ccd1ff5b
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2026-09-03 09:29:42 +0545

    fix: update comment types to use MongoId for consistency and add owner field

--- src/types/Services/comment.ts
+++ src/types/Services/comment.ts
@@ -1,11 +1,21 @@
-import type { Types } from "mongoose";
+import type { MongoId } from "../id.js";
 
 export type CreateComment = {
   content: string;
-  video: Types.ObjectId | string;
-  parentComment?: Types.ObjectId | string;
+  video: MongoId;
+  parentComment?: MongoId;
+  owner: MongoId;
 };
 
 export type UpdateComment = {
   content: string;
 };
+
+export type GetAllCommentsBase = {
+  userId: MongoId;
+  videoId: MongoId;
+  cursor: MongoId | null; // The cursor is the ID of the last comment fetched in the previous request
+};
+
+export type GetAllComments = GetAllCommentsBase &
+  ({ parentComment: MongoId } | { parentComment?: never });
