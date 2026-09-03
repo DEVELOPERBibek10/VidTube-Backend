1e968ec73602278fa4a3feef5cbe3dcde56c9b98
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2026-09-03 09:13:28 +0545

    refactor: improve validation middleware type handling and structure

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -1,10 +1,11 @@
-import type { Response, NextFunction } from "express";
-import z, { ZodError, ZodObject } from "zod";
+import type { NextFunction } from "express";
+import { ZodError, type ZodObject } from "zod";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ZodType } from "zod";
-import type { TypedRequest } from "../types/request.js";
+import type { AuthTypedRequest, TypedRequest } from "../types/request.js";
 import { ApiError } from "../utils/ApiError.js";
-import type { ParsedQs } from "qs";
+import { type ParamsDictionary } from "express-serve-static-core";
+import { type ParsedQs } from "qs";
 
 type RequestSchema = ZodObject<{
   body?: ZodType;
@@ -13,35 +14,37 @@ type RequestSchema = ZodObject<{
 }>;
 
 export const validation = (schema: RequestSchema) =>
-  asyncHandler(async (req: TypedRequest, res: Response, next: NextFunction) => {
-    try {
-      const parseData = await schema.parseAsync({
-        body: req.body,
-        params: req.params,
-        query: req.query,
-      });
-      req.body = parseData.body;
-      (req as any).params = parseData.params;
-      req.query = parseData.query as ParsedQs;
-      next();
-    } catch (error) {
-      if (error instanceof ZodError) {
-        const validationIssues = error.issues.map((issue) => ({
-          field: issue.path.join("."),
-          message: issue.message,
-        }));
+  asyncHandler(
+    async (req: TypedRequest | AuthTypedRequest, _, next: NextFunction) => {
+      try {
+        const parseData = await schema.parseAsync({
+          body: req.body,
+          params: req.params,
+          query: req.query,
+        });
+        req.body = parseData.body as Record<string, any>;
+        req.params = parseData.params as ParamsDictionary;
+        req.query = parseData.query as ParsedQs;
+        next();
+      } catch (error) {
+        if (error instanceof ZodError) {
+          const validationIssues = error.issues.map((issue) => ({
+            field: issue.path.join("."),
+            message: issue.message,
+          }));
 
-        const summaryMessage = validationIssues
-          .map((it) => `${it.field}: ${it.message}`)
-          .join(", ");
+          const summaryMessage = validationIssues
+            .map((it) => `${it.field}: ${it.message}`)
+            .join(", ");
 
-        throw new ApiError(
-          400,
-          "VALIDATION_ERROR",
-          summaryMessage || error.message,
-          validationIssues
-        );
+          throw new ApiError(
+            400,
+            "VALIDATION_ERROR",
+            summaryMessage || error.message,
+            validationIssues
+          );
+        }
+        throw error;
       }
-      throw error;
     }
-  });
+  );
