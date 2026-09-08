b068498fe6abf272a52c5de7ed432ae80536f359
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2026-09-08 14:27:57 +0545

    fix: update server log message to improve clarity on running port

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ async function server() {
     const database = await Promise.all([connectDB(), connectRedis()]);
     if (database.every((db) => db)) {
       const server = app.listen(port, () => {
-        console.log(`Server running on http://localhost:${port}`);
+        console.log(`Server running on port: ${port}`);
       });
       server.on("error", (err) => {
         console.error("EXPRESS SERVER FAILURE: Could not bind to port :", err);
