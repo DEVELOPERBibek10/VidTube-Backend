47373b73fb551d10b9469213ed65cb63afca895d
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2026-09-14 19:12:21 +0545

    feat:add video service document types

--- src/types/Services/video.ts
+++ src/types/Services/video.ts
@@ -11,3 +11,27 @@ export interface VideoUpdate {
   description?: string;
   isPublished?: boolean;
 }
+
+export interface VideoDocument {
+  _id: MongoId;
+  title: string;
+  description: string;
+  duration: number;
+  views: number;
+  isPublished: boolean;
+  owner: MongoId;
+  likes: number;
+  comments: number;
+  videoFile: { url: string; publicId: string };
+  thumbnail: { url: string; publicId: string };
+  createdAt: Date;
+  updatedAt: Date;
+  creator?: {
+    username: string;
+    avatar: { url: string; publicId: string };
+    subscribers: number;
+  };
+  isLiked?: boolean;
+  isSubscribed?: boolean;
+  watchTime?: number | null;
+}
