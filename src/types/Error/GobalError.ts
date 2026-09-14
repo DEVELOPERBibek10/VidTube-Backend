8eafd8cce554c45edd2dab3487eb76160e085184
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2026-09-14 19:12:21 +0545

    fix:remove unsafe global error any type

--- src/types/Error/GobalError.ts
+++ src/types/Error/GobalError.ts
@@ -1,12 +1,1 @@
-import type { Error as MongooseError } from "mongoose";
-import type { MulterError } from "multer";
-import type { ApiError } from "../../utils/ApiError.js";
-
-export type GlobalError =
-  | ApiError
-  | MongooseError.ValidationError
-  | MongooseError.CastError
-  | MulterError
-  | SyntaxError
-  | Error
-  | any;
+export type GlobalError = Error;
