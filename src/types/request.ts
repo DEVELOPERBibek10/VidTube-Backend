3a92dd5d3214aa0ea3aeb9f77e06e951eddc0c4d
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2026-09-14 19:12:22 +0545

    feat:type nullable request sections and files

--- src/types/request.ts
+++ src/types/request.ts
@@ -4,11 +4,14 @@ import type { ParsedQs } from "qs";
 import type { MongoId } from "./id.js";
 
 export interface TypedRequest<
-  TBody = Record<string, any>,
+  TBody,
   TParams extends Params = Params,
-> extends Omit<Request, "body" | "params"> {
+  TFiles = Express.Multer.File[] | Record<string, Express.Multer.File[]> | null,
+> extends Omit<Request, "body" | "params" | "files" | "file"> {
   body: TBody;
   params: TParams;
+  files: TFiles;
+  file: Express.Multer.File | null;
 }
 
 interface UserRequest {
@@ -29,15 +32,16 @@ interface UserRequest {
 }
 
 export interface AuthTypedRequest<
-  TBody = Record<string, any>,
+  TBody,
   TFile extends Express.Multer.File | Express.Multer.File[] | null =
     Express.Multer.File | Express.Multer.File[],
-  TParams extends Params | null = Params | null,
-  TQuery extends ParsedQs = ParsedQs,
-> extends Omit<Request, "body" | "files" | "params" | "query"> {
+  TParams extends Params | null = Params,
+  TQuery extends ParsedQs | null = ParsedQs,
+> extends Omit<Request, "body" | "files" | "file" | "params" | "query"> {
   body: TBody;
   user: UserRequest;
   params: TParams;
   query: TQuery;
   files: TFile;
+  file: Express.Multer.File | null;
 }
