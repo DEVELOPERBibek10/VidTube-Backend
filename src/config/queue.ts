04c96c8b66331d7a30cc4ab9bd2cab1d14d7fd82
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2026-09-19 12:58:06 +0545

    remove: delete CleanUpQueue configuration from queue.ts

--- src/config/queue.ts
+++
@@ -1,9 +0,0 @@
-import { Queue } from "bullmq";
-
-const CleanUpQueue = new Queue("cleanup", {
-  connection: {
-    host: process.env.REDIS_URL || "localhost:6379",
-  },
-});
-
-export default CleanUpQueue;
