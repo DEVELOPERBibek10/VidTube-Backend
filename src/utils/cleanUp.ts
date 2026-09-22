2ade07505391b5bf82faec5420bb42886579938c
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2026-09-22 23:12:56 +0545

    add: implement cleanUp function for batch deletion of related data (video related)

---
+++ src/utils/cleanUp.ts
@@ -0,0 +1,61 @@
+import { Comment } from "../models/comment.model.js";
+import { Like } from "../models/like.model.js";
+import { Playlist } from "../models/playlist.model.js";
+import { WatchHistory } from "../models/watchHistory.model.js";
+import type { MongoId } from "../types/id.js";
+
+const BATCH_SIZE = 1000;
+const BATCH_DELAY_MS = 2500;
+
+function wait(duration: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
+
+async function cleanUp(deletionId: MongoId): Promise<void> {
+  while (true) {
+    const [likes, comments, watchHistories, playlists] = await Promise.all([
+      Like.find({ likable: deletionId }, { _id: 1 }).limit(BATCH_SIZE).lean(),
+      Comment.find({ video: deletionId }, { _id: 1 }).limit(BATCH_SIZE).lean(),
+      WatchHistory.find({ video: deletionId }, { _id: 1 })
+        .limit(BATCH_SIZE)
+        .lean(),
+      Playlist.find({ videos: deletionId }, { _id: 1 })
+        .limit(BATCH_SIZE)
+        .lean(),
+    ]);
+    if (
+      likes.length === 0 &&
+      comments.length === 0 &&
+      watchHistories.length === 0 &&
+      playlists.length === 0
+    ) {
+      break;
+    }
+    const likeIds = likes.map((like) => like._id);
+    const commentIds = comments.map((comment) => comment._id);
+    const watchHistoryIds = watchHistories.map(
+      (watchHistory) => watchHistory._id
+    );
+    const playlistIds = playlists.map((playlist) => playlist._id);
+    await Promise.all([
+      Like.deleteMany({ _id: { $in: likeIds } }),
+      Comment.deleteMany({ _id: { $in: commentIds } }),
+      WatchHistory.deleteMany({ _id: { $in: watchHistoryIds } }),
+      Playlist.updateMany(
+        { videos: { $in: playlistIds } },
+        { $pull: { videos: deletionId } }
+      ),
+    ]);
+
+    if (
+      likes.length === BATCH_SIZE ||
+      comments.length === BATCH_SIZE ||
+      watchHistories.length === BATCH_SIZE ||
+      playlists.length === BATCH_SIZE
+    ) {
+      await wait(BATCH_DELAY_MS);
+    }
+  }
+}
+
+export default cleanUp;
