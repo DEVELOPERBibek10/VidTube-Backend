373154ba2237eddd981b4ce29c83902a88f60c79
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2026-09-26 19:42:43 +0545

    feat: add routes for likes, comments, and watch history in app

--- src/app.ts
+++ src/app.ts
@@ -22,9 +22,15 @@ import userRouter from "./routes/users.routes.js";
 import globalErrorHandler from "./middlewares/error.middleware.js";
 import videoRouter from "./routes/videos.routes.js";
 import { healthRouter } from "./routes/health.routes.js";
+import likeRouter from "./routes/likes.routes.js";
+import commentRouter from "./routes/comments.routes.js";
+import watchHistoryRouter from "./routes/watchHistory.routes.js";
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/like", likeRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/watch-history", watchHistoryRouter);
 app.use("/api/v1", healthRouter);
 app.use(globalErrorHandler);
 
